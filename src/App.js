cfc94ad63065ad8de193eba46a7d8cd84e414527
Gabriel Duarte <gabrield.developer@gmail.com>
2022-09-09 01:06:50 -0300

    chore: creates basic components structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
