6df1fe2bb325fb83a21b1347634e602102f83bbd
Gabriel Duarte <gabrield.developer@gmail.com>
2022-09-10 20:46:11 -0300

    feat(Login): creates login form basic structure

--- src/Components/Login/Form/LoginForm.js
+++ src/Components/Login/Form/LoginForm.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import pathValues from '../../../Enviroment/PathValues';
 import styles from './LoginForm.module.css';
 
 const LoginForm = () => {
-  return (
-    <div>LoginForm</div>
-  )
-}
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-export default LoginForm
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    fetch(pathValues.API_BASE_URL + 'jwt-auth/v1/token', {
+      method: 'POST',
+      headers: pathValues.HEADERS,
+      body: JSON.stringify({ username, password }),
+    })
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={({ target }) => setUsername(target.value)}
+          value={username}
+        />
+        <input
+          type="password"
+          onChange={({ target }) => setPassword(target.value)}
+          value={password}
+        />
+        <button>Entrar</button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
---
+++ src/Enviroment/PathValues.js
@@ -0,0 +1,6 @@
+const pathValues = {
+  API_BASE_URL: 'https://dogsapi.origamid.dev/json/',
+  HEADERS: { 'Content-Type': 'application/json' }
+}
+
+export default pathValues;
