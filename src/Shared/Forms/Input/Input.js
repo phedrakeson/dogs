c13d22ba1e52d0604fb7aa2da7f8ba737773004e
Gabriel Duarte <gabrield.developer@gmail.com>
2022-09-10 21:10:27 -0300

    feat(Input): creates and stylizes generic input component

--- src/Shared/Forms/Input/Input.js
+++ src/Shared/Forms/Input/Input.js
@@ -3,9 +3,10 @@ import styles from './Input.module.css'
 
 const Input = ({ label, type, name }) => {
   return (
-    <div>
-      <label htmlFor={name}>{label}</label>
+    <div className={styles.wrapper}>
+      <label className={styles.label} htmlFor={name}>{label}</label>
       <input id={name} name={name} type={type} className={styles.input} />
+      <p className={styles.error}>Error</p>
     </div>
   )
 }
