b221229f03a52a5a84ad9376927654855f34b7ab
Gabriel Duarte <gabrield.developer@gmail.com>
2022-09-11 10:30:08 -0300

    feat(Login): implements sign up page and adds strong password validation

--- src/Components/Login/Create/LoginCreate.js
+++ src/Components/Login/Create/LoginCreate.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import Button from '../../../Shared/Forms/Button/Button';
+import Input from '../../../Shared/Forms/Input/Input';
+import { useForm } from '../../../Shared/Hooks/useForm';
 import styles from './LoginCreate.module.css'
 
 const LoginCreate = () => {
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm('password');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <div>LoginCreate</div>
+    <section className="animeLeft">
+      <h1 className='title'>Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
   )
 }
 
--- src/Shared/Hooks/useForm.js
+++ src/Shared/Hooks/useForm.js
@@ -4,6 +4,10 @@ const types = {
   email: {
     regex: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
     message: 'Preencha um email válido'
+  },
+  password: {
+    regex: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+    message: 'A senha precisa ter 1 caracter maiúsculo, 1 minúsculo, 1 dígito e 1 caracter especial. Com no mínimo 8 caracteres.'
   }
 }
 
