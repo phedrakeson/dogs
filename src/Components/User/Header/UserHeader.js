1965ef6d2b35ed03ed9be9228b35c246c9f60e29
Gabriel Duarte <gabrield.developer@gmail.com>
2022-09-11 11:47:04 -0300

    chore(User): stylizes user menu header and adds dynamic title

--- src/Components/User/Header/UserHeader.js
+++ src/Components/User/Header/UserHeader.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 import { UserHeaderNav } from './Nav/UserHeaderNav'
 import styles from './UserHeader.module.css'
 
 export const UserHeader = () => {
+  const [title, setTitle] = React.useState('');
+  const location = useLocation();
+
+  React.useEffect(() => {
+    switch (location.pathname) {
+      case '/account/post':
+        setTitle('Post your photo');
+        break;
+      case '/account/stats':
+        setTitle('Stats');
+        break;
+      default:
+        setTitle('My Account');
+        break;
+    }
+  }, [location])
   return (
-    <header>
-      <h1 className='title'>Título</h1>
+    <header className={styles.header}>
+      <h1 className='title'>{title}</h1>
       <UserHeaderNav />
     </header>
   )
