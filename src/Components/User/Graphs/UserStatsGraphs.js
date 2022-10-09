1430952f3dbbaef2d2c14e5f36eb16798f5813a5
Gabriel Duarte <gabrield.developer@gmail.com>
2022-10-09 00:00:31 -0300

    feat(User): implements victory lib graphs for statistics page

--- src/Components/User/Graphs/UserStatsGraphs.js
+++ src/Components/User/Graphs/UserStatsGraphs.js
@@ -1,19 +1,45 @@
 import React from 'react'
 import styles from './UserStatsGraphs.module.css'
+import { VictoryPie, VictoryChart, VictoryBar } from 'victory';
 
 export const UserStatsGraphs = ({ data }) => {
   const [graph, setGraph] = React.useState([]);
   const [total, setTotal] = React.useState(0);
 
   React.useEffect(() => {
+    const graphData = data.map(item => {
+      return {
+        x: item.title,
+        y: Number(item.acessos)
+      }
+    })
 
-    setTotal(data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b))
+    setTotal(data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b));
+    setGraph(graphData);
   }, [data]);
 
   return (
     <section className={`${styles.graph} animeLeft`}>
-      <div className={styles.total}>
+      <div className={`${styles.total} ${styles.graphItem}`}>
         <p>Visits: {total}</p>
+      </div>
+      <div className={styles.graphItem}>
+        <VictoryPie data={graph} innerRadius={50} padding={{ top: 20, bottom: 80, right: 80, left: 80 }} style={{
+          data: {
+            fillOpacity: .9,
+            stroke: '#fff',
+            strokeWidth: 2
+          },
+          labels: {
+            fontSize: 14,
+            fill: '#333'
+          }
+        }} />
+      </div>
+      <div className={styles.graphItem}>
+        <VictoryChart>
+          <VictoryBar alignment='start' data={graph} />
+        </VictoryChart>
       </div>
     </section>
   )
