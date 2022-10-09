4c6b928a2545da905de2ca18eb98e03bf7586580
Gabriel Duarte <gabrield.developer@gmail.com>
2022-10-09 00:23:41 -0300

    refactor(User): changes stats page title to statistics

--- src/Components/User/Header/UserHeader.js
+++ src/Components/User/Header/UserHeader.js
@@ -13,7 +13,7 @@ export const UserHeader = () => {
         setTitle('Post your photo');
         break;
       case '/account/stats':
-        setTitle('Stats');
+        setTitle('Statistics');
         break;
       default:
         setTitle('My Photos');
